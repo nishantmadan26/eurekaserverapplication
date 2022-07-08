3b39b0dd4b7a00937308874bff04f3b19f539726
Nishant Madan <nishant.madan@onpier.de>
2022-07-08 12:17:23 +0200

    initial commit

---
+++ src/test/java/com/server/eurekaserverapplication/EurekaserverapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.eurekaserverapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
